57f5a706c0583e005383f71db297ce5e561cc8e8
LoisClinton <loisclinton@hotmail.co.uk>
2023-10-17 11:11:03 +0100

    finished Farmers list

--- index.js
+++ index.js
@@ -1,49 +1,36 @@
 // Import Classes Here
-
-
-
-
-
-
-
-
-
+let Product = require("./classes/Product.js");
+let Cart = require("./classes/Cart.js");
+let Customer = require("./classes/Customer.js");
+let Auth = require("./classes/Auth.js");
 
 // DO NOT EDIT BELOW THIS LINE
 try {
-    module.exports = {
-        Product,
-    }
-} catch(e){
-
-}
+  module.exports = {
+    Product,
+  };
+} catch (e) {}
 
 try {
-    module.exports = {
-        Product,
-        Cart
-    }
-} catch(e){
-
-}
+  module.exports = {
+    Product,
+    Cart,
+  };
+} catch (e) {}
 
 try {
-    module.exports = {
-        Product,
-        Cart,
-        Customer
-    }
-} catch(e){
-
-}
+  module.exports = {
+    Product,
+    Cart,
+    Customer,
+  };
+} catch (e) {}
 
 try {
-    module.exports = {
-        Product,
-        Cart,
-        Customer,
-        Auth
-    }
-} catch(e){
-
-}
+  module.exports = {
+    Product,
+    Cart,
+    Customer,
+    Auth,
+  };
+} catch (e) {}
